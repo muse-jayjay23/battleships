ea02822a06ef9103e22be7e2421cac5bf32c809f
muse-jayjay23 <joshuamjones23@hotmail.com>
2023-02-02 13:42:54 +0000

    Create def print_board to display game board to terminal

--- run.py
+++ run.py
@@ -7,9 +7,19 @@ GUESS_BOARD = [[' '] * 8 for x in range(8)]
 let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
-#main functions
-def board():
-    pass
+"""
+main functions
+"""
+
+
+def print_board(board):
+    print('   A B C D E F G H')
+    print('   ---------------')
+    row_number = 1
+    """This iterates through each row in the board and joins the pipes"""
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 
 def create_ships():
@@ -23,7 +33,11 @@ def get_ship_location():
 def count_hit_ships():
     pass
 
+
+"""
 #game process
 create_ships()
 turns = 10
 while turns > 0:
+"""
+
