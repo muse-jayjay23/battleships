33b70614787c44dc827b3a81c88d4bb01d34330c
muse-jayjay23 <joshuamjones23@hotmail.com>
2023-02-02 13:25:59 +0000

    Build basic outline of game including functions and its process as well as order of running functions.

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+#Boards for both player on comp
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
+
+
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+
+#main functions
+def board():
+    pass
+
+
+def create_ships():
+    pass
+
+
+def get_ship_location():
+    pass
+
+
+def count_hit_ships():
+    pass
+
+#game process
+create_ships()
+turns = 10
+while turns > 0:
